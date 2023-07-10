d6e4aa69ecf4c6ae8405b8a44c6407cff24070bf
Alex <ansechin2012@ya.ru>
2023-07-10 21:46:31 +0300

    4

---
+++ 6/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число и выдает является ли число четным
+// 4 -> да
+
+Console.Write("Напишите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0){
+    Console.WriteLine("Это четное число");
+}
+else Console.WriteLine("Это нечетное число");
