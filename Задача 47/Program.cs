50e27a2bd1b481b1ed0c3bdbe93b27278194abd0
Alex <ansechin2012@ya.ru>
2023-07-26 11:27:49 +0300

    Задача 47 двумерный массив вещественных чисел

---
+++ Задача 47/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+double[,] numbers = new double[5, 5];
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        numbers[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;;
+    }
+}
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        Console.Write(numbers[i, j] + " ");
+    }
+    Console.WriteLine();
+}
